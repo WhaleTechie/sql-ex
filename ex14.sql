fc9707f69d23adbf5501ac28d26d0a03d6a4eaf4
Lidia Krinova <lkrinova@gmail.com>
2021-02-26 18:16:18 +0300

    Create ex14.sql

---
+++ ex14.sql
@@ -0,0 +1,6 @@
+# For the ships in the Ships table that have at least 10 guns, get the class, name, and country.
+
+SELECT Ships.class, Ships.name, Classes.country
+FROM Ships JOIN Classes
+ON Ships.class = Classes.class 
+WHERE numGuns >= 10
