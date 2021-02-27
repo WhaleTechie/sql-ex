c42d7d5a6ebec4d300a9412ff79c34ac44407010
Lidia Krinova <lkrinova@gmail.com>
2021-02-28 00:01:56 +0300

    Create ex25.sql

---
+++ ex25.sql
@@ -0,0 +1,27 @@
+# Find the printer makers also producing PCs with the lowest RAM capacity and the highest processor speed of all PCs having the lowest RAM capacity.
+#Result set: maker.
+
+SELECT DISTINCT maker
+FROM product
+WHERE model IN (
+SELECT model
+FROM pc
+WHERE ram = (
+  SELECT MIN(ram)
+  FROM pc
+  )
+AND speed = (
+  SELECT MAX(speed)
+  FROM pc
+  WHERE ram = (
+   SELECT MIN(ram)
+   FROM pc
+   )
+  )
+)
+AND
+maker IN (
+SELECT maker
+FROM product
+WHERE type='printer'
+)
