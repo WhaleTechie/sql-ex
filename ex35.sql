6becca8e50f81e10b1a55b760e615c4ccd21d613
Lidia Krinova <lkrinova@gmail.com>
2021-02-28 19:10:00 +0300

    Create ex35.sql

---
+++ ex35.sql
@@ -0,0 +1,7 @@
+# Find models in the Product table consisting either of digits only or Latin letters (A-Z, case insensitive) only.
+# Result set: model, type.
+
+SELECT model, type
+FROM Product
+WHERE upper(model) NOT like '%[^A-Z]%'
+OR model not like '%[^0-9]%'
