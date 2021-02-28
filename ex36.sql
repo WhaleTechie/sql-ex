db923a41cce50c7c2f551bc6c160b09457153100
Lidia Krinova <lkrinova@gmail.com>
2021-03-01 00:45:15 +0300

    Update ex36.sql

--- ex36.sql
+++ ex36.sql
@@ -9,3 +9,12 @@ SELECT Outcomes.ship FROM Outcomes
 JOIN Classes 
 ON Outcomes.ship = Classes.class 
 WHERE outcomes.ship = classes.class) a
+
+
+# NOTE
+# Translation of LIKE cases
+
+LIKE %[0-9]% - a string with digits (string contains digits)
+NOT LIKE %[0-9]% a string without digits (string doesnt contain digits)
+NOT LIKE %[^0-9]% - a string only with digits (string does not contain not digits)
+LIKE %[^0-9]% - string countains not digits
